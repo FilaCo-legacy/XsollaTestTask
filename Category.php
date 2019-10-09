72d819a6aeb4c2a902acbb45b61775eba1405a53
FilaCo <dvchizha@edu.hse.ru>
2019-10-09 06:01:26 +0500

    Определил интерфейс категории и комментарии к нему

--- Category.php
+++ Category.php
@@ -3,8 +3,15 @@
 
 namespace NewsFeedModule;
 
-
-interface INewsCategory
+// Интерфейс категории
+interface Category
 {
-
+    // Получить идентификатор категории из БД
+    function getId(): int;
+    // Получить название категории
+    function getName(): string;
+    // Получить список новостей, относящихся к категории
+    function getNews(): array;
+    // Привязать новость к категории
+    function bindNews(News $news): void;
 }
