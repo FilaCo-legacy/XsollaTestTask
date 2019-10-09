c3f9dd240a89017cb138f9c4a860911eb469af19
FilaCo <dvchizha@edu.hse.ru>
2019-10-09 06:38:57 +0500

    Добавил определения для модулей взаимодействия и новостей

--- InteractionModule.php
+++ InteractionModule.php
@@ -3,8 +3,33 @@
 
 namespace NewsFeedModule;
 
-
+/**
+ * Интерфейс для модуля взаимодействий новостной ленты
+ * @package NewsFeedModule
+ */
 interface InteractionModule
 {
+    /**
+     * Проверить, может ли пользователь связывать новости и категории
+     * @param User $user - ссылка на пользователя
+     * @param Category $category - ссылка на категорию
+     * @param News $news - ссылка на новость
+     * @return bool - возвращает true, если пользователь может привязать категорию к новости, иначе - false
+     */
+    function canBindNewsToCategory(User $user, Category $category, News $news): bool;
 
+    /**
+     * Привязать новость к категории
+     * @param User $user - ссылка на пользователя
+     * @param Category $category - ссылка на категорию
+     * @param News $news - ссылка на новость
+     */
+    function bindNewsToCategory(User $user, Category $category, News $news): void;
+
+    /**
+     * Получить все новости, которые которые удовлетворяют хотя бы одной категории
+     * @param array $categories - список категорий
+     * @return array - список новостей, удовлетворяющих хотя бы одной категории
+     */
+    function getNewsByCategories(array $categories): array;
 }
--- NewsModule.php
+++ NewsModule.php
@@ -3,8 +3,30 @@
 
 namespace NewsFeedModule;
 
-
-class NewsModule
+/**
+ * Интерфейс модуля для работы с постами новостной ленты
+ * @package NewsFeedModule
+ */
+interface NewsModule
 {
+    /**
+     * Проверить, имеет ли юзер права на добавление новостей
+     * @param User $user - ссылка на пользователя
+     * @return bool - true, если пользователь имеет права добавлять новости, иначе false
+     */
+    function canAddNews(User $user): bool;
 
+    /**
+     * Добавить новый пост в ленту
+     * @param User $author - автор поста
+     * @param string $text - текст новости
+     * @return News - ссылка на новый объект новости
+     */
+    function addNews(User $author, string $text): News;
+
+    /**
+     * Получить все новости из ленты, сущетсвующие на момент вызова
+     * @return array - массив новостей
+     */
+    function getAllNews(): array;
 }
