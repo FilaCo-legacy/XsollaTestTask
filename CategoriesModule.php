d14ef9049cfaf78e751d9438d54c8e68d09e56f1
FilaCo <dvchizha@edu.hse.ru>
2019-10-09 06:28:25 +0500

    Добавил определение модулю категорий и добавил комментарии

--- CategoriesModule.php
+++ CategoriesModule.php
@@ -3,8 +3,29 @@
 
 namespace NewsFeedModule;
 
-
+/**
+ * Интерфейс к модулю новостной ленты, который отвечает за работу с категориями
+*/
 interface CategoriesModule
 {
+    /**
+     * Проверить, имеет ли юзер права на добавление категорий
+     * @param User $user - ссылка на пользователя
+     * @return bool - true, если пользователь имеет права добавлять категории, иначе false
+     */
+    function canAddCategories(User $user): bool;
 
+    /**
+     * Добавить новую категорию в список
+     * @param User $author - автор категории
+     * @param string $categoryName - название категории
+     * @return Category - ссылка на новый объект категории
+     */
+    function addCategory(User $author, string $categoryName): Category;
+
+    /**
+     * Вернуть список всех категорий
+     * @return array - массив, содержащий все категории, существующие на момент вызова
+     */
+    function getAllCategories(): array;
 }
