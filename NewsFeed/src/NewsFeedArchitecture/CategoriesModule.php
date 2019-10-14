116751a2cd09e3b82b575816b0355852e890802a
FilaCo <dvchizha@edu.hse.ru>
2019-10-14 13:28:01 +0500

    Поменял структуру модуля категорий

--- NewsFeed/src/NewsFeedArchitecture/CategoriesModule.php
+++ NewsFeed/src/NewsFeedArchitecture/CategoriesModule.php
@@ -4,7 +4,51 @@
 namespace NewsFeedModule\Architecture;
 
 
-class CategoriesModule
-{
+use App\Entity\Category;
+use App\Entity\User;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use http\Exception;
 
+class CategoriesModule implements CategoriesModuleInterface
+{
+    private $categoryRepository;
+
+    public function getCategoryRepository():CategoryRepository{
+        return $this->getCategoryRepository();
+    }
+
+    /**
+     * Проверить, имеет ли юзер права на добавление категорий
+     * @param User $user - ссылка на пользователя
+     * @return bool - true, если пользователь имеет права добавлять категории, иначе false
+     */
+    public function canAddCategories(User $user): bool
+    {
+        // TODO: Implement canAddCategories() method.
+    }
+
+    /**
+     * Добавить новую категорию в список
+     * @param User $author - автор категории
+     * @param string $categoryName - название категории
+     * @return Category - ссылка на новый объект категории
+     * @throws \Exception
+     */
+    public function addCategory(User $author, string $categoryName): Category
+    {
+        if (!$this->canAddCategories($author))
+            throw new \Exception("Пользователь не обладает правами на добавление категорий");
+
+
+    }
+
+    /**
+     * Вернуть список всех категорий
+     * @return Collection - коллекция, содержащая все категории, существующие на момент вызова
+     */
+    function getAllCategories(): Collection
+    {
+        // TODO: Implement getAllCategories() method.
+    }
 }
